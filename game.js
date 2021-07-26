1ac16f8e386668342f9158a5ec03e14c943fc547
Anganam <anganamnkili@gmail.com>
2021-07-26 20:51:59 +0200

    commit

--- game.js
+++ game.js
@@ -11,12 +11,14 @@ function nextSequence(){
 
     gamePattern.push(randomChosenColour);
 
-    $("#"+ randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+    animatePress(randomChosenColour);
 
     // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     // audio.play();
 
-    switch (randomChosenColour) {
+    /*switch (randomChosenColour) {
         case "red":
             var redColor = new Audio("sounds/red.mp3");
             redColor.play();
@@ -37,15 +39,55 @@ function nextSequence(){
         default:
             console.log(randomChosenColour)
             break;
-    }
+    }*/
 
     
 } 
 
-/*$("#btn").click(function(){
-    var userChosenColour =  this.btn.attr("id");
+$(".btn").click(function(){
+    var userChosenColour =  $(this).attr("id");
 
     userClickedPattern.push(userChosenColour);
-})*/
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour)
+
+    //var audio = new Audio("sounds/" + userChosenColour + ".mp3");
+    //audio.play();
+
+    /*switch (userChosenColour) {
+        case "red":
+            var redColor = new Audio("sounds/red.mp3");
+            redColor.play();
+            break;
+        case "blue":
+            var blueColor = new Audio("sounds/blue.mp3");
+            blueColor.play();
+            break;
+         case "green":
+            var greenColor = new Audio("sounds/green.mp3");
+            greenColor.play();
+            break;
+         case "yellow":
+            var yellowColor = new Audio("sounds/yellow.mp3");
+            yellowColor.play();
+            break;
+    
+        default:
+            console.log(randomChosenColour)
+            break;
+    }*/
+
+    //console.log(userClickedPattern);
+})
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
 
+function animatePress(currentColour) {
+    $("#"+ currentColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
